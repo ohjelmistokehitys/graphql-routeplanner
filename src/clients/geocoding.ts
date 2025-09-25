24c72f210a1588095d2d2e9a947977bf39c85159
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-09-25 09:35:54 +0300

    Added solution

--- src/clients/geocoding.ts
+++ src/clients/geocoding.ts
@@ -1,6 +1,16 @@
 import type { AddressSearchResponse } from "../types/GeocodingApi";
 
 export async function addressSearch(query: string): Promise<AddressSearchResponse | null> {
-    console.log(`Address search not implemented. Returning null for ${query}.`);
-    return null;
+    const response = await fetch(`/geocoding/v1/search?text=${encodeURIComponent(query)}&size=1`, {
+        headers: {
+            'Accept': 'application/json'
+        },
+        method: 'GET'
+    });
+
+    if (!response.ok) {
+        throw new Error('Could not fetch address, got ' + response.status);
+    }
+
+    return await response.json();
 }
--- src/clients/routing.ts
+++ src/clients/routing.ts
@@ -3,6 +3,66 @@ import type { Itinerary, RoutingResponse } from "../types/RoutingApi";
 
 
 export async function planItinerary(from: GeoJsonFeature, to: GeoJsonFeature, count = 3): Promise<Itinerary[]> {
-    console.log(`Itinerary planning not implemented. Returning empty array.`);
-    return [];
+    const query = `{
+        plan(
+            from: ${formatCoord(from)}
+            to: ${formatCoord(to)}
+            numItineraries: ${count}
+        ) {
+            itineraries {
+                start
+                end
+                walkTime
+                walkDistance
+                legs {
+                    from {
+                        name
+                        lat
+                        lon
+                    }
+                    to {
+                        name
+                        lat
+                        lon
+                    }
+                    startTime
+                    endTime
+                    mode
+                    duration
+                    distance
+                    route {
+                        shortName
+                        longName
+                    }
+                }
+            }
+        }
+    }`;
+
+    // this request goes to our local proxy, which adds the authentication headers
+    // and forwards the request to the actual digitransit API:
+    const response = await fetch('/routing/v2/hsl/gtfs/v1', {
+        headers: { 'Content-Type': 'application/graphql' },
+        method: 'POST',
+        body: query
+    });
+
+    if (!response.ok) {
+        console.error(response);
+        throw new Error('GraphQL query failed.');
+    }
+
+    const routingResponse: RoutingResponse = await response.json();
+    return routingResponse.data.plan.itineraries;
+}
+
+/**
+ * Formats a GeoJsonFeature coordinate into a string, that is accepted
+ * by the digitransit GraphQL API.
+ *
+ * @param p The GeoJsonFeature to format.
+ * @returns The formatted coordinate string.
+ */
+function formatCoord(p: GeoJsonFeature) {
+    return `{ lat: ${p.geometry.coordinates[1]}, lon: ${p.geometry.coordinates[0]} }`;
 }
