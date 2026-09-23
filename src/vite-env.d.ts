a7805ca92b60e0d5f9fa79733b7568aedf225264
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2026-09-23 12:12:06 +0000

    Updated dependencies and added npm and devcontainer configurations

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_DIGITRANSIT_SUBSCRIPTION_KEY: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
